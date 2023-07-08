66d03e9f1b9aadc2903f8b21808973a5c013a313
StasenkO1337 <78676050+StasenkO1337@users.noreply.github.com>
2023-07-08 15:50:07 +0500

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,11 +4,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        // по аналогии с примером все делаем
-        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
-        Cat beanCat = (Cat) applicationContext.getBean("Cat");
 
-        System.out.println(bean.getMessage());
-        System.out.println(beanCat.getMessage());
+        HelloWorld hello1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld hello2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Hello bean - " + (hello1 == hello2));
+        System.out.println("Cat bean - " + (cat1 == cat2));
     }
 }
